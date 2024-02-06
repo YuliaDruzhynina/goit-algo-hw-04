fd67d208b75e96d7bf31c1ebe8026c090eb6cb68
Poshtaklon <152329594+YuliaDruzhynina@users.noreply.github.com>
2024-02-06 16:04:55 +0200

    change salary class

--- goit-algo-04_01.py
+++ goit-algo-04_01.py
@@ -3,16 +3,26 @@ from pathlib import Path
 
 def total_salary(path):
     try:
-        with open(path) as file:
-            file_text = file.readlines()            #read file return list
+        with open(path) as file:          
+            file_text = str(file.readlines())                   
+            #salary = []
             pattern = r"\d+"
-            salary = re.findall(pattern, file_text) #return list                                                    #print(salary)
-            total = sum(salary)                    #return int
-            avarage = total/ len(salary)           
+            salary = re.findall(pattern, file_text)                                                                       
+            salary_int = [int(num) for num in salary]
+            total = sum(salary_int)                           
+            average = int(total/ len(salary_int)) if len(salary_int) > 0 else 0                      
 
     except FileNotFoundError:       
-       
-        return (total, avarage)       
+        print('file not found')
+    except Exception as e:
+         return None
+    return (total, average)                        
+
 
 total, average = total_salary(r"path/to/salary_file.txt")
+
+# if total is not None and average is not None:
 print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+# else:
+#     print("Error occurred.")
+
