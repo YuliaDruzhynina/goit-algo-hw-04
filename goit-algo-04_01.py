c61c9e537ab06d10d07d820f7c2b97038cfd6ff2
Poshtaklon <152329594+YuliaDruzhynina@users.noreply.github.com>
2024-02-15 13:56:16 +0200

    Update goit-algo-04_01.py
    change except None None

--- goit-algo-04_01.py
+++ goit-algo-04_01.py
@@ -4,25 +4,28 @@ from pathlib import Path
 def total_salary(path):
     try:
         with open(path) as file:          
-            file_text = str(file.readlines())                   
-            #salary = []
-            pattern = r"\d+"
-            salary = re.findall(pattern, file_text)                                                                       
-            salary_int = [int(num) for num in salary]
-            total = sum(salary_int)                           
-            average = int(total/ len(salary_int)) if len(salary_int) > 0 else 0                      
+            file_text = file.read() 
+            print(file_text)
+
+        pattern = r"\d+"
+        salary = re.findall(pattern, file_text)    
+        salary_int = list(map(int, salary)) 
+        total = sum(salary_int)                      
+        average = int(total/ len(salary_int)) if len(salary_int) > 0 else 0                    
 
     except FileNotFoundError:       
         print('file not found')
+        #return None, None
     except Exception as e:
-         return None
-    return (total, average)                        
+         return None, None
+    
+    return total, average                        
 
 
-total, average = total_salary(r"path/to/salary_file.txt")
+total, average = total_salary(r"/Users/juliia/Desktop/GoIt_hw/Text-04_01.txt")
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
 
 # if total is not None and average is not None:
-print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
-# else:
+
 #     print("Error occurred.")
 
